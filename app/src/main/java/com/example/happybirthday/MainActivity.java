236b0dba6d2092156c32ebdc8de21d371a8272ed
FachryIkhsal <fachryikhsal890@gmail.com>
2021-03-20 16:37:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.happybirthday;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
